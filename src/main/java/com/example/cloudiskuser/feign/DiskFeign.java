e483afb7288500958dc6471f814d3ea76aa4d0e5
jeffcahn <2623768443@qq.com>
2025-07-11 16:54:33 +0800

    add dockerfile

--- src/main/java/com/example/cloudiskuser/feign/DiskFeign.java
+++ src/main/java/com/example/cloudiskuser/feign/DiskFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "disk-service", url = "http://localhost:8080")
+@FeignClient(name = "disk-service", url = "${disk.init:''}")
 public interface DiskFeign {
 
     @PostMapping("/init/user")
--- src/main/java/com/example/cloudiskuser/feign/SpaceFeign.java
+++ src/main/java/com/example/cloudiskuser/feign/SpaceFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "space-service", url = "http://localhost:8082")
+@FeignClient(name = "space-service", url = "${space.create:''}")
 public interface SpaceFeign {
 
 
